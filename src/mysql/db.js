a7f64fb60924b0767e5330d05849f2cea74d73ef
ikhwaDEV1528 <ikhwanmarditya123@student.esaunggul.ac.id>
2026-07-23 22:49:29 +0700

    Remove unused local mysql api routes

--- src/mysql/db.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2/promise";
-
-export const db = await mysql.createConnection({
-  host: "localhost",     // kalau pake XAMPP biasanya localhost
-  user: "root",          // default user XAMPP
-  password: "",          // kosong (kalau kamu belum set password)
-  database: "belajardb", // nama database yang udah kamu buat
-});
